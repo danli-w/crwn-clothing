bb7c65b1e6163d756033554473ca58917fe5a3e3
danli-w <wdlforeverlove@gmail.com>
2019-11-30 14:40:48 -0600

    finish signin page and styling signin form and signin button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component = {HomePage}/>
         <Route exact path='/shop' component = {ShopPage}/>
+        <Route exact path='/signin' component = {SignInAndSignUpPage}/>
       </Switch>
   
     </div>
